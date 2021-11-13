55d264e630671ad4b612c18222bc4bbb642cfff2
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-11-13 14:39:12 +0200

    все

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import Login from './Login/Login.js'
+// import logo from './logo.svg';
+import React from 'react';
+
+import Login from './Login/Login.js';
 
 function App() {
-  return (
-   <Login/>
-    
-  );
+  return <Login />;
 }
 
 export default App;
