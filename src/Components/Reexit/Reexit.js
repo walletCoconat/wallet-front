511b9bf061095323f6cd1fe9983ade53eb854227
nkpn <illinmikita@gmail.com>
2021-11-20 16:30:29 +0200

    исправил мелкие баги, удалил css файлы, добавил scss

--- src/Components/Reexit/Reexit.js
+++ src/Components/Reexit/Reexit.js
@@ -1,39 +1,54 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React from 'react';
+import { useDispatch } from 'react-redux';
 
-import style from './Reexit.module.css';
+import style from './Reexit.module.scss';
 import sprite from '../../images/sprite.svg';
 import * as authOperation from '../../redux/auth/authOperation';
 
+import Button from '../Button/Button';
+import Close from '../Close';
 
-import Button from "../Button/Button";
-import Close from "../Close";
+const Reexit = ({ toggleIsVisible }) => {
+  const dispatch = useDispatch();
+  const onClick = () => {
+    toggleIsVisible();
+    dispatch(authOperation.logOut());
+  };
 
-const Reexit = ({toggleIsVisible}) => {
+  return (
+    <div className={style.Modale}>
+      <div className={style.Container}>
+        <Close
+          onClick={onClick}
+          style={{
+            border: '1px solid #ffffff',
+            backgroundColor: '#ffffff',
+          }}
+        >
+          <svg className={style.Icon}>
+            <use xlinkHref={`${sprite}#icon-close`}></use>
+          </svg>
+        </Close>
 
-    const dispatch = useDispatch();
-    const onClick=() => {
-        toggleIsVisible();
-        dispatch(authOperation.logOut())
-         }
+        <h1>Вы действительно хотите выйти?</h1>
 
-
-    return <div className={style.Modale}>
-    <div className={style.Container}>
-    <Close onClick={onClick}>
-        <svg className={style.Icon}><use xlinkHref={`${sprite}#icon-close`}></use></svg>
-    </Close>
-  
-    <h1>Вы действительно хотите выйти?</h1>
-
-    <ul>
-        <li className={style.Item} ><Button onClick={onClick}>Да</Button>
-        </li>
-        <li><Button onClick={()=>{toggleIsVisible()}}>Нет</Button></li>
-    </ul>
-        </div>
+        <ul>
+          <li className={style.Item}>
+            <Button onClick={onClick}>Да</Button>
+          </li>
+          <li>
+            <Button
+              onClick={() => {
+                toggleIsVisible();
+              }}
+            >
+              Нет
+            </Button>
+          </li>
+        </ul>
+      </div>
     </div>
-
-}
+  );
+};
 
 export default Reexit;
