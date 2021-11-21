95ca90ae93508b7bd2718bddba31d23e8a2300f4
nkpn <illinmikita@gmail.com>
2021-11-21 18:13:26 +0200

    Таблицы для мобилки и десктопа (только верстка)

--- src/Components/Dashboard/DashboardHome.js
+++ src/Components/Dashboard/DashboardHome.js
@@ -1,37 +1,48 @@
-import TotalBalance from '../TotalBalance/index'
-import HomeTab from '../HomeTab/index'
+import TotalBalance from '../TotalBalance/index';
+import HomeTab from '../HomeTab/index';
 import './Dashboard.scss';
 import Media from 'react-media';
 import MainInfo from './Dashboard_main_info';
+import MobileTab from '../HomeTab/MobileTab';
 
-const Home = () => <div >
-        <Media queries= {{
-            small: '(max-width: 767px)',
-            medium: '(min-width: 768px)',
-        }}>
+const Home = () => (
+  <div>
+    <Media
+      queries={{
+        small: '(max-width: 767px)',
+        medium: '(min-width: 768px)',
+      }}
+    >
+      {matches => (
+        <div>
+          {/* For mobile screen */}
+          {matches.small && (
+            <>
+              <div className="Dashboard_balance">
+                {' '}
+                <TotalBalance />{' '}
+              </div>
+              <div className="Dashboard_table">
+                {' '}
+                <MobileTab />{' '}
+              </div>
+            </>
+          )}
 
-            {matches => (
-                <div>
-                    {/* For mobile screen */}
-                    {matches.small && 
-                        <>
-                            <div className='Dashboard_balance'> <TotalBalance/> </div>
-                            <div className='Dashboard_table'> MOBILE TABLET </div>
-                            
-                        </>}
-
-                    {/* For tablet screen*/}
-                    {matches.medium && 
-                        <div className='home_container'> 
-                            <MainInfo/> 
-                            <div className='Dashboard_table'> <HomeTab/> </div>
-                           
-                        </div>
-                    }
-                </div>
-            )}
-        </Media> 
-    </div>
+          {/* For tablet screen*/}
+          {matches.medium && (
+            <div className="home_container">
+              <MainInfo />
+              <div className="Dashboard_table">
+                {' '}
+                <HomeTab />{' '}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </Media>
+  </div>
+);
 
 export default Home;
-
