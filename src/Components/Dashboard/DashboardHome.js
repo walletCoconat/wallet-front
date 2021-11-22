7fea7ca8fc94c1b539d90e67877321f6c9bc0f00
Yulia Shcherban <caribywest@gmail.com>
2021-11-22 10:40:28 +0200

    добавила логику работы с сервером

--- src/Components/Dashboard/DashboardHome.js
+++ src/Components/Dashboard/DashboardHome.js
@@ -1,14 +1,35 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchTransactions, fetchStatistic, addTransaction } from '../../redux/finance/financeOperations'
 import TotalBalance from '../TotalBalance/index'
 import HomeTab from '../HomeTab/index'
+import MobileTab from '../HomeTab/index'
 import './Dashboard.scss';
 import Media from 'react-media';
 import MainInfo from './Dashboard_main_info';
 import ButtonAddTransaction from '../ButtonAddTransaction';
 
-const Home = () => 
+// const uixdata = new Date("2021.11.10").getTime();
 
+// const newTransaction = {
+//     type: false,
+//     category: 'education',
+//     sum: 500,
+//     date: uixdata,
+// }
+
+const Home = () => {
+    const dispatch = useDispatch()
+
+    // useEffect(() => dispatch(fetchTransactions()), []);
+    // useEffect(() => dispatch(addTransaction(newTransaction)), []);
+    // useEffect(() => dispatch(fetchStatistic()), []);
+    // console.log('data-UNIX', newTransaction.date)
+    // console.log('data-UNIX', uixdata)
+
+    return (
     <div >
-        <Media queries= {{
+        <Media queries={{
             small: "(max-width: 767px)",
             medium: "(min-width: 768px)",
         }}>
@@ -16,25 +37,26 @@ const Home = () =>
             {matches => (
                 <div>
                     {/* For mobile screen */}
-                    {matches.small && 
+                    {matches.small &&
                         <>
-                            <div className='Dashboard_balance'> <TotalBalance/> </div>
-                            <div className='Dashboard_table'> MOBILE TABLET </div>
+                            <div className='Dashboard_balance'> <TotalBalance /> </div>
+                            <div className='Dashboard_table'> <MobileTab/> </div>
                         </>}
 
                     {/* For tablet screen*/}
-                    {matches.medium && 
-                        <div className='home_container'> 
-                            <MainInfo/> 
-                            <div className='Dashboard_table'> <HomeTab/> </div>
-                            <ButtonAddTransaction/>
+                    {matches.medium &&
+                        <div className='home_container'>
+                            <MainInfo />
+                            <div className='Dashboard_table'> <HomeTab /> </div>
+                            <ButtonAddTransaction />
                         </div>
                     }
                 </div>
             )}
-        </Media> 
-    </div>
-   
+        </Media>
+        </div>
+    )
+}
 
 export default Home;
 
--- src/redux/finance/financeReducer.js
+++ src/redux/finance/financeReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
-// import { initState } from '../../Components/HomeTab/initState'
+import { fetchTransactions, addTransaction, fetchStatistic } from './financeOperations'
 
 
 const balanceReducer = createReducer(28000, {
@@ -8,10 +8,41 @@ const balanceReducer = createReducer(28000, {
 })
 
 const financeDataReducer = createReducer([], {
-
+  [fetchTransactions.fulfilled]: (state, { payload }) => [...state, ...payload],
+  // добавть проверку на пустой массив при запросе првой страницы
+  // не забудь про пагинацию
+  [addTransaction.fulfilled]: (state, { payload }) => [...state, payload],
 })
 
+const StatisticReducer = createReducer(null, {
+  [fetchStatistic.fulfilled]: (_, { payload }) => payload,
+})
+
+const error = createReducer(null, {
+  [fetchTransactions.rejected]: (_, { payload }) => payload,
+  [fetchTransactions.pending]: () => false,
+  [addTransaction.rejected]: (_, { payload }) => payload,
+  [addTransaction.pending]: () => false,
+  [fetchStatistic.rejected]: (_, { payload }) => payload,
+  [fetchStatistic.pending]: () => false,
+});
+
+const loading = createReducer(false, {
+  [fetchTransactions.pending]: () => true,
+  [fetchTransactions.rejected]: () => false,
+  [fetchTransactions.fulfilled]: () => false,
+  [addTransaction.pending]: () => true,
+  [addTransaction.rejected]: (_, { payload }) => payload,
+  [addTransaction.fulfilled]: () => false,
+  [fetchStatistic.pending]: () => true,
+  [fetchStatistic.rejected]: (_, { payload }) => payload,
+  [fetchStatistic.fulfilled]: () => false,
+});
+
 export default combineReducers({
-    totalBalance: balanceReducer,
-    data: financeDataReducer,
+  totalBalance: balanceReducer,
+  financeData: financeDataReducer,
+  statistic: StatisticReducer,
+  error,
+  loading,
 });
